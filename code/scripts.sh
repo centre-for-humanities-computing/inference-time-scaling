c0e6b9b36181a8dbc34703992ff7759b1096627a
LegrandNico <nicolas.legrand@cas.au.dk>
2025-11-28 10:23:39 +0100

    packaging

--- code/scripts.sh
+++ code/scripts.sh
@@ -1,8 +1,9 @@
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-models=("Qwen/Qwen2.5-0.5B-Instruct" "Qwen/Qwen2.5-1.5B-Instruct" "Qwen/Qwen2.5-3B-Instruct" "Qwen/Qwen2.5-7B-Instruct" "meta-llama/Llama-3.2-1B-Instruct" "meta-llama/Llama-3.2-3B-Instruct")
-methods=("kl-divergence")
-datasets=("math500" "gsm8k-en" "gsm8k-da")
+#models=("Qwen/Qwen2.5-0.5B-Instruct" "Qwen/Qwen2.5-1.5B-Instruct" "Qwen/Qwen2.5-3B-Instruct" "meta-llama/Llama-3.2-1B-Instruct" "meta-llama/Llama-3.2-3B-Instruct")
+#models=("meta-llama/Llama-3.2-1B-Instruct" "meta-llama/Llama-3.2-3B-Instruct")
+#methods=("kl-divergence")
+#datasets=("gsm8k-en" "gsm8k-da")
 
 # save output to a log file
 exec > output.log 2>&1
@@ -10,21 +11,28 @@ exec > output.log 2>&1
 # Forward Pass -------------------------------------------------------------------------
 # for model in "${models[@]}"; do
 #     for dataset in "${datasets[@]}"; do
-#         uv run python main.py --method "forward-pass" --model_id "$model" --dataset "$dataset" --n_proposal 4 # --overwrite True
+#         uv run python main.py --method "forward-pass" --model_id "$model" --dataset "$dataset" --n_proposal 4 --overwrite True
 #     done
 # done
 
 
 # Inferene time Scaling ----------------------------------------------------------------
-for dataset in "${datasets[@]}"; do
-   for model in "${models[@]}"; do
-       for method in "${methods[@]}"; do
-           for proposal in 2 4 8; do
-               uv run python main.py --method "$method" --n_proposal "$proposal" --model_id "$model" --dataset "$dataset"
-           done
-       done
-   done
-done
+# for dataset in "${datasets[@]}"; do
+#    for model in "${models[@]}"; do
+#        for method in "${methods[@]}"; do
+#            for proposal in 2 4 8; do
+#                uv run python main.py --method "$method" --n_proposal "$proposal" --model_id "$model" --dataset "$dataset" --overwrite True
+#            done
+#        done
+#    done
+# done
+
+# Early stop ITS
+uv run python main.py --method "forward-pass" --model_id "Qwen/Qwen2.5-3B-Instruct" --n_proposal "1" --dataset "math500r" --overwrite True
+uv run python main.py --method "early-stop" --max_sampling_steps "1" --n_proposal "16" --model_id "Qwen/Qwen2.5-3B-Instruct" --dataset "math500r"
+uv run python main.py --method "early-stop" --max_sampling_steps "2" --n_proposal "16" --model_id "Qwen/Qwen2.5-3B-Instruct" --dataset "math500r"
+uv run python main.py --method "early-stop" --max_sampling_steps "5" --n_proposal "16" --model_id "Qwen/Qwen2.5-3B-Instruct" --dataset "math500r"
+uv run python main.py --method "early-stop" --max_sampling_steps "40" --n_proposal "16" --model_id "Qwen/Qwen2.5-3B-Instruct" --dataset "math500r"
 
 # screen -S its  # create a new screen session named "its"
 # Press Ctrl + A, then D (mnemonic: Detach).
